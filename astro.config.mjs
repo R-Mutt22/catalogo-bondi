bd86c06af5159e3d179a1400fd1f503b403271db
Matías Zelarayán <matiaszelarayandev@gmail.com>
2026-05-11 23:26:13 -0300

    Actualizando

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  //  Modo estático (SSG)
+  output: 'static',
+  
+  //  Configuración de imágenes (sin 'format')
+  image: {
+    domains: [],
+    cacheDir: './.astro/cache/image',
+  },
+
+  //  Configuración para optimización
+  vite: {
+    ssr: {
+      external: ['sharp']
+    }
+  }
+});
